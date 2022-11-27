de884dac1b365234ea977b2417b1523b158ce276
Peter-Sciberras <sciberrasp@yahoo.com.au>
2022-11-27 16:50:39 +1100

    img linked & css edit for positioning

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ document.querySelector("body").appendChild(h1);
 const h2 = document.createElement("h2");
 h2.textContent = "Peter Sciberras";
 document.querySelector("body").appendChild(h2);
+const p1 = document.createElement("p1");
+p1.textContent = "I am an electrical engineer serving in the biomedical engineering service space. My dedication focuses on improving the healthcare sector in whatever way I am able"
+document.querySelector("body").appendChild(p1);

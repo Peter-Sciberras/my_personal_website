e712cebe26e3c1dad12b532d1335d05f4820e086
Peter-Sciberras <sciberrasp@yahoo.com.au>
2022-11-27 18:56:44 +1100

    added extra js, contact form & additional css editing

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const h1 = document.createElement("h1");
 h1.textContent = "Biomedical Engineer";
 document.querySelector("body").appendChild(h1);
@@ -8,3 +7,12 @@ document.querySelector("body").appendChild(h2);
 const p1 = document.createElement("p1");
 p1.textContent = "I am an electrical engineer serving in the biomedical engineering service space. My dedication focuses on improving the healthcare sector in whatever way I am able"
 document.querySelector("body").appendChild(p1);
+const p2 = document.createElement("p2");
+p2.textContent = "Contact Me";
+document.querySelector("body").appendChild(p2);
+const p3 = document.createElement("p3");
+p3.textContent = "Sydney based, with over 15 years experience in the biomedical service space serving 5+ years in operations management. Specialist in medical lasers, medical gases, anaesthesia and ventilation equipment and studying software engineering. I believe that one can never stop learning and evolving.";
+document.querySelector("body").appendChild(p3);
+const p4 = document.createElement("p4");
+p4.textContent = "Thank you for visiting";
+document.querySelector("body").appendChild(p4);
